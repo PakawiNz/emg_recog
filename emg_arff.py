26efb2bcbadcff899b5bb11c5c35f1d03449e651
giftfig <mi_ni_gift_@hotmail.com>
2015-03-02 16:57:39 +0700

    add min,max genarff

--- emg_arff.py
+++ emg_arff.py
@@ -1,4 +1,4 @@
-from emg_automation import feature_extr,get_rawdata
+# from emg_automation import feature_extr,get_rawdata
 from emg_autoweka import autoWEKA
 
 def getPath_raw(filename):
@@ -13,10 +13,27 @@ def getPath_arff(ctype,filename):
 def getPath_train(filename):
 	return '4train/%s.txt'%(filename)
 
-def rawpick(pick,ctype,filename): # pick = constance of each number of record each type 
+def storepick_arff(pick,ctype,filename): # pick = constance of each number of record each type 
 	arfffile = getPath_arff(ctype, filename)
 	arff = open(arfffile,"w")
+	data = []
+	import glob
+	stores = glob.glob('1store/*.csv')
+	for store in stores:
+		print store
+		print pick
+		lines = open(store,'r').readlines()
+		for p in xrange(0,min(pick,len(lines))):
+			data.append(lines[p])
+
+		fd_seperate = zip(*map(lambda x: map(float,x.split(',')[:-1]), data))
+		minfd = map(min,fd_seperate)
+		maxfd = map(max,fd_seperate)
+
 	# head
+	arff.write("%% min: %s \n"%(minfd))
+	arff.write("%% max: %s \n"%(maxfd))
+	
 	arff.write("@relation '%s'\n"%(filename))
 	for x in xrange(1,9):
 		arff.write("@attribute FD%d numeric\n"%x)
@@ -32,20 +49,17 @@ def rawpick(pick,ctype,filename): # pick = constance of each number of record ea
 	
 	# data
 	arff.write("@data\n")
-	import glob
-	stores = glob.glob('1store/*.csv')
-	for store in stores:
-		print store
-		print pick
-		lines = open(store,'r').readlines()
-		for p in xrange(0,min(pick,len(lines))):
-			arff.write(lines[p])
+
+	for line in data:
+		arff.write('%s\n'%line)
+
+
 	arff.close()
 
 	return arfffile
 
 
-def rawtoarff(ctype,filename): #type : 0=one variable, 1=one hot
+def fd_store(ctype,filename): #type : 0=one variable, 1=one hot
 	elementFFT = [0,128,4,8,0]
 	rawdata = get_rawdata(getPath_raw(filename))
 	features = feature_extr(elementFFT, rawdata, False)
@@ -71,6 +85,7 @@ def rawtoarff(ctype,filename): #type : 0=one variable, 1=one hot
 	store.close()
 	return features
 
+
 if __name__ == '__main__':
 	import glob
 	ctype = 0
@@ -80,19 +95,20 @@ if __name__ == '__main__':
 	# for x in txt:
 	# 	filename = x.split('.')[0].split('/')[1] # for mac
 	# 	# filename = x.split('.')[0].split('\\')[1] # for windows
-	# 	rawtoarff(ctype, filename)
+	# 	fd_store(ctype, filename)
 
 	#pickDataset /
 	# picks = [5000,10000,15000,20000,25000,30000]
 	# for pick in picks:
-	# 	rawpick(pick/25,ctype,"data"+str(pick))
+	# 	storepick_arff(pick/25,ctype,"data"+str(pick))
+	storepick_arff(10,ctype,"datatestpick"+str(10))
 	
 	#autoDataTest
 	# autoWEKA('Exp0','2arff/data10000-0.arff')
 
 	# autoWEKA('Exp1','2arff/data10000-0.arff')
 	
-	autoWEKA('Exp2','2arff/data10000-0.arff')
+	# autoWEKA('Exp2','2arff/data10000-0.arff')
 	# autoWEKA('Exp2','2arff/data15000-0.arff')
 	# autoWEKA('Exp2','2arff/data20000-0.arff')
 	# autoWEKA('Exp2','2arff/data25000-0.arff')
--- emg_autoweka.py
+++ emg_autoweka.py
@@ -1,6 +1,5 @@
 import itertools
-import re
-import os
+import re,sys,os
 import datetime
 
 #Exp0 correct command /
@@ -16,51 +15,69 @@ import datetime
 # EPOCH 	= [500] 
 # MOMENTUM 	= [0.1]
 # LEARNING_RATE = [0.1]
-# HIDDEN0 	= [8,7,6,5]
+# HIDDEN0 	= [9,8,7,6,5,4,3,2,1]
 # HIDDEN1 	= [7,6,5]
 
 #Exp2 vary learning rate & momentum & hidden0
-N_FOLD 	= [10]
-EPOCH 	= [500] 
-MOMENTUM 	= [0.05,0.1,0.2,0.3,0.4,0.5]
-LEARNING_RATE = [0.05,0.1,0.2,0.3,0.4,0.5]
-HIDDEN0 	= [] #<<<<<<<<<<<<<<<<form Exp1
-HIDDEN1 	= [] #<<<<<<<<<<<<<<<<form Exp1
+# N_FOLD 	= [10]
+# EPOCH 	= [500] 
+# MOMENTUM 	= [0.05,0.1,0.2,0.3,0.4,0.5]
+# LEARNING_RATE = [0.05,0.1,0.2,0.3,0.4,0.5]
+# HIDDEN0 	= [8]#<<<<<<<<<<<<<<<<form Exp1
+# HIDDEN1 	= [6]#<<<<<<<<<<<<<<<<form Exp1
 
 #Ex3 vary EPOCH
-# N_FOLD 	= [10,20,50,100] 
-# EPOCH 	= [500,1000,2000,5000,800,10000]
-# MOMENTUM 	= [] #<<<<<<<<<<<<<<<<form Exp2
-# LEARNING_RATE = [] #<<<<<<<<<<<<<<<<form Exp2
-# HIDDEN0 	= []
-# HIDDEN1 	= []
+N_FOLD 	= [10] 
+EPOCH 	= [500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2500,3000]#[500,1000,2000,5000]
+MOMENTUM 	= [0.1]#<<<<<<<<<<<<<<<<form Exp2
+LEARNING_RATE = [0.1]#<<<<<<<<<<<<<<<<form Exp2
+HIDDEN0 	= [8]
+HIDDEN1 	= [6]
 
-AUTOMATION0 = [N_FOLD,EPOCH,MOMENTUM,LEARNING_RATE,HIDDEN0,HIDDEN1]
-AUTOMATION1 = [N_FOLD,EPOCH,MOMENTUM,LEARNING_RATE,HIDDEN0]
+#Ex4 vary Dataset amount
+# N_FOLD 	= []#<<<<<<<<<<<<<<<<form Exp3
+# EPOCH 	= []#<<<<<<<<<<<<<<<<form Exp3
+# MOMENTUM 	= [0.1]
+# LEARNING_RATE = [0.1]
+# HIDDEN0 	= [8]
+# HIDDEN1 	= [6]
 
-WEKA_PATH = '-classpath "/Users/Gift/Downloads/meka-1.7.5/lib/weka.jar"' #for mac
-# WEKA_PATH = '-classpath "C:\Program Files\Weka-3-6\weka.jar"'#for windows
+AUTOMATION = [N_FOLD,EPOCH,MOMENTUM,LEARNING_RATE,HIDDEN0,HIDDEN1]
+# AUTOMATION = [N_FOLD,EPOCH,MOMENTUM,LEARNING_RATE,HIDDEN0]
+
+if sys.platform.startswith('win'):
+	WEKA_PATH = '-classpath "C:\Program Files\Weka-3-6\weka.jar"' #for windows
+elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+	WEKA_PATH = '-classpath "/Users/Gift/Downloads/meka-1.7.5/lib/weka.jar"' #for mac
+elif sys.platform.startswith('darwin'):
+	WEKA_PATH = '-classpath "/Users/Gift/Downloads/meka-1.7.5/lib/weka.jar"' #for mac
+else:
+	raise EnvironmentError('Unsupported class path')
 WEKA_CLASS = 'weka.classifiers.functions.MultilayerPerceptron'
 
 W0 = 0
 X0 = 1
 
 def autoWEKA(exp,filename):
-
-	fn = filename.split('.')[0].split('/')[1]	
-	afile = open('3stat/'+exp+'/'+fn+'-'+exp+'.csv','a+')
+	if sys.platform.startswith('win'):
+		fn = x.split('.')[0].split('\\')[1] # for windows
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		fn = filename.split('.')[0].split('/')[1]	
+	elif sys.platform.startswith('darwin'):
+		fn = filename.split('.')[0].split('/')[1]	
+	else:
+		raise EnvironmentError('Unsupported path')
 	count = 0
 
-	cartesian0 = list(itertools.product(*AUTOMATION0)) #Epx1
-	cartesian1 = list(itertools.product(*AUTOMATION1))
+	cartesian = list(itertools.product(*AUTOMATION))
 	print "weka variation = %d"%len(cartesian)
 
 	print "START WEKA"
 
-	# for var in cartesian0 :
-	for var in cartesian1 :
-		# WEKA_OPTION = ' -L %.2f -M %.2f -N %d -V 0 -S 0 -E 20 -H %d,%d -B -C -R -v -x %d'%(var[3],var[2],var[1],var[4],var[5],var[0])
-		WEKA_OPTION = ' -L %.2f -M %.2f -N %d -V 0 -S 0 -E 20 -H %d -B -C -R -v -x %d'%(var[3],var[2],var[1],var[4],var[0])
+	for var in cartesian :
+		afile = open('3stat/'+exp+'/'+fn+'-'+exp+'.csv','a+')
+		WEKA_OPTION = ' -L %.2f -M %.2f -N %d -V 0 -S 0 -E 20 -H %d,%d -B -C -R -v -x %d'%(var[3],var[2],var[1],var[4],var[5],var[0])
+		# WEKA_OPTION = ' -L %.2f -M %.2f -N %d -V 0 -S 0 -E 20 -H %d -B -C -R -v -x %d'%(var[3],var[2],var[1],var[4],var[0])
 
 		WEKA_CMD = " ".join(["java",WEKA_PATH,WEKA_CLASS,WEKA_OPTION,"-t",filename])
 		count += 1
@@ -81,15 +98,34 @@ def autoWEKA(exp,filename):
 
 		table = re.search(r'=== Detailed Accuracy By Class ===\s+([\w\t -]+)\s+([\d.\s]+).*', result)
 		lines = re.split('\s+',table.group(2))
-		data = [[],[],[],[],[],[],[],[],[]]
+		data = [[],[],[],[],[],[],[]]
 		for i,word in enumerate(lines):
-			data[i%9].append(word)
+			data[i%7].append(word)
 
-
-
-		# csvdata = ",".join(map(str,list(var)+[datetime.datetime.now() - start]+[ACCU.group(2)]+[MAE.group(1)]+[RMSE.group(1)]+[RAE.group(1)]+[RRSE.group(1)]+data[2:5]+[WEKA_CMD]))
-		csvdata = ",".join(map(str,list(var)+[]+[datetime.datetime.now() - start]+[ACCU.group(2)]+[MAE.group(1)]+[RMSE.group(1)]+[RAE.group(1)]+[RRSE.group(1)]+data[2:5]+[WEKA_CMD]))
-		afile.write(csvdata)
+		csvdata = ",".join(map(str,list(var)+[datetime.datetime.now() - start]+[ACCU.group(2)]+[EPE]+[MAE.group(1)]+[RMSE.group(1)]+[RAE.group(1)]+[RRSE.group(1)]+data[2]+data[3]+data[4]+[WEKA_OPTION]))
+		# csvdata = ",".join(map(str,list(var)+["-"]+[datetime.datetime.now() - start]+[ACCU.group(2)]+[EPE]+[MAE.group(1)]+[RMSE.group(1)]+[RAE.group(1)]+[RRSE.group(1)]+data[2]+data[3]+data[4]+[WEKA_OPTION]))
+		afile.write(csvdata+'\n')
+		afile.close()
 	
-	afile.close()
 	return 1
+
+if __name__ == '__main__':
+	import glob
+	ctype = 0
+	
+	#autoDataTest
+	# autoWEKA('Exp0','2arff/data10000-0.arff')
+
+	# autoWEKA('Exp1','2arff/data10000-0.arff')
+	
+	# autoWEKA('Exp2','2arff/data10000-0.arff')
+
+	autoWEKA('Exp3','2arff/data10000-0.arff')
+
+	# autoWEKA('Exp4','2arff/data10000-0.arff')
+	# autoWEKA('Exp4','2arff/data15000-0.arff')
+	# autoWEKA('Exp4','2arff/data20000-0.arff')
+	# autoWEKA('Exp4','2arff/data25000-0.arff')
+	# autoWEKA('Exp4','2arff/data30000-0.arff')
+	# autoWEKA('Exp4','2arff/data35000-0.arff')
+	# autoWEKA('Exp4','2arff/data5000-0.arff')
